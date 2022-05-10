bb26a0197aa5db98d7876e5e3f6f6c276a13c523
SCWeeegeeday <104513094+SCWeeegeeday@users.noreply.github.com>
2022-05-10 10:15:32 -0400

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,4 +1,11 @@
 #For Bot
 TOKEN = ''
-BotStatus = "Example Bot"
+BotStatus = "Penguin"
 Prefix = "."
+#url is more for testing, its to test that the web server responds to POST and GET. 
+# if you change hostname or the port, update the URL.
+# you will need to chage it to connect the esp32.
+# use ipconfig or something, too lazy to specify exaclty what.
+URL = "http://localhost:8080"
+hostname = "localhost"
+port = "8080"
